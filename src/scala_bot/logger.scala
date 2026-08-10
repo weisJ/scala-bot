a1b6a21648fe0207bc17c82f05db301f3a98e49a
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2026-08-10 22:10:20 +0200

    Store logfile

--- src/scala_bot/logger.scala
+++ src/scala_bot/logger.scala
@@ -1,6 +1,8 @@
 package scala_bot.logger
 
 import scala.Console._
+import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.file.{Files, Path, StandardOpenOption}
 
 object LogLevel:
 	inline val Off = 0
@@ -10,11 +12,34 @@ object LogLevel:
 
 object Logger:
 	@volatile var runtimeLevel: Int = LogLevel.Info
+	@volatile private var logFile: Option[Path] = None
 
 	def setLevel(level: Int): Unit =
 		runtimeLevel = level
 
 	def level = runtimeLevel
+
+	def setFile(path: Option[Path]): Unit = synchronized:
+		if logFile != path then
+			path.foreach: file =>
+				val parent = file.getParent
+				if parent != null then
+					Files.createDirectories(parent)
+					()
+				// A new table log starts empty; revisiting the same path preserves it.
+				Files.writeString(file, "", UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+				()
+			logFile = path
+
+	def writeToFile(message: String): Unit = synchronized:
+		logFile.foreach: file =>
+			try
+				Files.writeString(file, s"$message${System.lineSeparator}", UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND)
+				()
+			catch
+				case error: Exception =>
+					System.err.println(s"Failed to write log file: ${error.getMessage}")
+					logFile = None
 
 object Log:
 	inline val compileTimePriority = 0		// Change this value to 3 to enable full debug logs
@@ -22,7 +47,9 @@ object Log:
 	inline def log(inline level: Int, inline msg: => String, inline colour: String = WHITE): Unit =
 		inline if level <= compileTimePriority then
 			if level <= Logger.runtimeLevel then
-				println(s"$colour$msg$RESET")
+				val message = msg
+				println(s"$colour$message$RESET")
+				Logger.writeToFile(message)
 		else
 			()
 
